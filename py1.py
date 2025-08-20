936034907f13909a432108317f409ceeb612f92b
Ridhi3004 <ridhi8588.beaift25@chitkara.edu.in>
2025-08-20 20:16:24 +0530

    Add files via upload

---
+++ py1.py
@@ -0,0 +1,53 @@
+a=55
+b=60
+c=a+b
+print(c)
+print("-----------")
+
+a=56.23
+b=65.45
+c=a+b
+print(c)
+print("-----------")
+
+a=562345815555
+b=451236789565
+c=a+b
+print(c)
+print("-----------")
+
+
+a=5
+b=6
+c=a+b
+print(c)
+print("-----------")
+
+
+a = int(input("Enter 1st number:"))
+b = int(input("Enter 2nd number:"))
+c = a+b
+print(c)
+print("-----------")
+
+
+e = float(input("Enter 1st number:"))
+f = float(input("Enter 2nd number:"))
+d = e+f
+print(d)
+print("-----------")
+
+
+A = str(input("Enter 1st number:"))
+B = str(input("Enter 2nd number:"))
+C =  A+B
+print(C)
+print("-----------")
+
+
+D = chr(int(input("Enter 1st number:")))
+E = chr(int(input("Enter 2nd number:")))
+F = D+E
+print(F)
+print("-----------")
+
