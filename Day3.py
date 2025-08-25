5880484a578ebac08187d0f09340c4a80d442acf
Ridhi3004 <ridhi8588.beaift25@chitkara.edu.in>
2025-08-25 23:21:24 +0530

    Add files via upload

---
+++ Day3.py
@@ -0,0 +1,52 @@
+# Looping
+for i in range(1,1001):
+    print("Numbers:",i)
+
+Fruits=["Apple","Mango","Cherry","Melon"]
+for x in Fruits:
+    print("I Like:",x)
+
+
+#WHILE LOOP
+i=1
+while i <=5:
+    print("Number:",i)
+    i+=1
+
+#FOR LOOP (Printing first 10 natural numbers)
+for i in range(1,11):
+    print(i)
+print("-----------")
+
+
+#WHILE LOOP
+i=1
+while i<=10:
+    print(i)
+    i+=1
+print("-----------")
+
+
+#TABLE OF 5
+for i in range(1, 11):
+    print(f"5 x {i} = {5*i}")
+print("-----------")
+
+
+i=1
+while i<=10:
+        print(f"5 x {i} = {5*i}")
+        i+=1
+print("-----------")
+
+#Print numbers from 1 to 100
+for i in range(1,101):
+    print("Numbers:",i)
+print("-----------")
+
+i=1
+while i<=100:
+     print(i)
+     i+=1
+print("-----------")
+
