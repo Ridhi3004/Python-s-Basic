248b08c8c87e2ac1705fb123c45ec4a712a4d0ee
Ridhi3004 <ridhi8588.beaift25@chitkara.edu.in>
2025-08-26 22:49:44 +0530

    Add files via upload

---
+++ print10natural.py
@@ -0,0 +1,12 @@
+#FOR LOOP (Printing first 10 natural numbers)
+for i in range(1,11):
+    print(i)
+print("-----------")
+
+
+#WHILE LOOP
+i=1
+while i<=10:
+    print(i)
+    i+=1
+print("-----------")
