808d693060d0e88c79b3cc79fc64c2b5e9e1455f
Ridhi3004 <ridhi8588.beaift25@chitkara.edu.in>
2025-08-26 22:48:59 +0530

    Add files via upload

---
+++ checkevenorodd.py
@@ -0,0 +1,6 @@
+# Write a program to check whether a given number is even or odd using the modulus (%) operator
+num=int(input("Enter a number:"))
+if num % 2==0:
+    print(num,"is even")
+else:
+    print(num,"is odd")
---
+++ remainderandquotient.py
@@ -0,0 +1,5 @@
+# Write a program to find the remainder and quotient when one number is divided by another
+a=int(input("Enter a number:"))
+b=int(input("Enter a number:"))
+print("Remainder:",a%b)
+print("Quotient:",a/b)
