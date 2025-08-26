54e489dfb82bbe0149b83d0b0c67c36a06500c2f
Ridhi3004 <ridhi8588.beaift25@chitkara.edu.in>
2025-08-26 10:50:28 +0530

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,54 @@
+# # ARITHMETIC OPERATORS
+# a=10
+# b=30
+# c=a+b
+# print("The sum is:",c)    #Sum
+
+# a=10
+# b=30
+# c=a-b
+# print("The difference is:",c)     #Difference
+
+# a=10
+# b=65.2
+# c=a*b
+# print("The result is:",c)      #Multiply
+
+# a=3
+# b=72.5
+# c=a/b
+# print("a divides b=",c)       #Divide
+
+# a=2
+# b=9
+# c=a%b
+# print("a % b=",c)       #Modulus
+
+
+
+# # ASSIGNMENT OPERATORS
+# a=23
+# b=43
+# print("a is equal to b :",a==b)
+
+# a=56
+# b=56
+# print("a is equal to b :",a==b)
+
+
+# # LOGICAL OPERATORS
+# a=50
+# b=45
+# print("a is greater thab b and a  is equal to b :",a>b and a==b)
+# a=50
+# b=45
+# print("a is greater than b and a is equal to b :",a>b or a==b)
+
+# a=int(input ("Enter a number"))
+# if not range (1,15) :
+#     print ("try agian")
+# else:
+#     print(a)
+a=10
+b=20
+print(not(a>b))
