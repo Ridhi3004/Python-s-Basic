7c472ba76e585f7a5cb742194f446ad915951448
Ridhi3004 <ridhi8588.beaift25@chitkara.edu.in>
2025-08-27 09:24:39 +0530

    numberswap.py

--- numberswap.py
+++ numberswap.py
@@ -3,4 +3,14 @@ n=int(input("Enter a number"))
 m=int(input("Enter a number"))
 print("Before swap: n =", n ,"m =",m)
 print("After swap: n =", m ,"m =",n)
+print("---------")
+
+
+a=5
+b=10
+print("Before swap: a =", a ,"b =",b)
+a=a+b
+b=a-b
+a=a-b
+print("After swap: a =", a ,"b =",b)
 
